25c61a972bfeb60d1b8d4b1f465d30bf149933b1
Joanna Biedroń <joanna.biedron@gmail.com>
2019-10-06 13:51:53 +0200

    Add js code - computerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-let computerMove= 'kameń';
-let playerMove= 'papier';
-printMessage ('Zagrałem '+computerMove+'! Jeśli zagrałeś '+playerMove+ ',to wyrgywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber==2){
+  computerMove='nożyce';
+}
+else if (randomNumber==3) {
+  computerMove='papier';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
