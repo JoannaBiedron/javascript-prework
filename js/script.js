da69fc7d0efd50b4af6b4b34c7137bbf85266255
Joanna Biedroń <joanna.biedron@gmail.com>
2019-10-07 20:25:13 +0200

    Add js code -function playGame and clearMessage

--- js/script.js
+++ js/script.js
@@ -1,95 +1,109 @@
+function playGame(playerInput){
 
-function getMoveName(argMoveId){
-  if(argMoveId == 1){
-    return 'kamień';
-  }
-  else if(argMoveId==2){
-    return 'papier';
-  }
-  else if (argMoveId==3) {
-    return 'nożyce';
-  }
-  else {
-    return 'nieznany ruch';
+  function clearMessages(){}
+
+  /*funkcja getMoveName */
+  function getMoveName(argMoveId){
+
+    if(argMoveId == 1){
+      return 'kamień';
+    }
+    else if(argMoveId==2){
+      return 'papier';
+    }
+    else if (argMoveId==3) {
+      return 'nożyce';
+    }
+    else {
+      return 'nieznany ruch';
     /*printMessage('TVARDY weź się nie wygłupiaj. Wybierz swój ruch: kamień, papier lub nożyce. Tu się nie gra kaloryferem, krzesłem ...ani śrubokrętem');
-*/
+    */
+    }
   }
-}
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+  /*Ruch komputera*/
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+  console.log('Wylosowana liczba to: ' + randomNumber);
 
-let argComputerMove = getMoveName (randomNumber);
-/*
-if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-else if(randomNumber==2){
-  computerMove='nożyce';
-}
-else if (randomNumber==3) {
-  computerMove='papier';
-}*/
+  let argComputerMove = getMoveName (randomNumber);
+  /*
+  if(randomNumber == 1){
+    computerMove = 'kamień';
+  }
+  else if(randomNumber==2){
+    computerMove='nożyce';
+  }
+  else if (randomNumber==3) {
+    computerMove='papier';
+  }
+  */
 
-printMessage('Ruch komputera to: ' + argComputerMove);
+  printMessage('Ruch komputera to: ' + argComputerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+  /*Ruch gracza*/
 
-console.log('Gracz wpisał: ' + playerInput);
+  /*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
 
-let argPlayerMove = getMoveName (playerInput);
-/*
-if(playerInput == '1'){
-  playerMove = 'kamień';
-}
-else if (playerInput=='2') {
-  playerMove='nożyce';
-}
-else if (playerInput=='3') {
-  playerMove='papier';
-}*/
+  console.log('Gracz wpisał: ' + playerInput);
 
-printMessage('Twój ruch to: ' + argPlayerMove);
+  let argPlayerMove = getMoveName (playerInput);
+  /*
+  if(playerInput == '1'){
+    playerMove = 'kamień';
+  }
+  else if (playerInput=='2') {
+    playerMove='nożyce';
+  }
+  else if (playerInput=='3') {
+    playerMove='papier';
+  }
+  */
 
-function displayResult(argComputerMove, argPlayerMove){
+  printMessage('Twój ruch to: ' + argPlayerMove);
 
-if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-  return('Ty wygrywasz!');
+  /*funkcja displayResult - wynik */
+
+  function displayResult(argComputerMove, argPlayerMove){
+
+    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+      return('Ty wygrywasz!');
+    }
+    else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+      return('Ty wygrywasz!');
+    }
+    else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+      return('Ty wygrywasz!');
+    }
+    else if( argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+      return('Remis!');
+    }
+    else if( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
+      return('Remis!');
+    }
+    else if( argComputerMove == 'papier' && argPlayerMove == 'papier'){
+      return('Remis!');
+    }
+    else if( argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+      return('Wygrywa komputer!');
+    }
+    else if( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+      return('Wygrywa komputer!');
+    }
+    else if( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+      return('Wygrywa komputer!');
+    }
+    else if( argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch'){
+      return('TVARDY weź się nie wygłupiaj. Wybierz swój ruch: kamień, papier lub nożyce. Tu się nie gra kaloryferem, krzesłem ...ani śrubokrętem');
+    }
+    else if( argComputerMove == 'nożyce' && argPlayerMove == 'nieznany ruch'){
+      return('TVARDY weź się nie wygłupiaj. Wybierz swój ruch: kamień, papier lub nożyce. Tu się nie gra kaloryferem, krzesłem ...ani śrubokrętem');
+    }
+    else if( argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch'){
+      return('TVARDY weź się nie wygłupiaj. Wybierz swój ruch: kamień, papier lub nożyce. Tu się nie gra kaloryferem, krzesłem ...ani śrubokrętem');
+    }
+  }
+  /*printMessage('Komputer zagrał '+argComputerMove+', a Ty '+argPlayerMove);*/
+  printMessage ('wynik: '+ displayResult(argComputerMove, argPlayerMove));
 }
-else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
-  return('Ty wygrywasz!');
-}
-else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
-  return('Ty wygrywasz!');
-}
-else if( argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
-  return('Remis!');
-}
-else if( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
-  return('Remis!');
-}
-else if( argComputerMove == 'papier' && argPlayerMove == 'papier'){
-  return('Remis!');
-}
-else if( argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
-  return('Wygrywa komputer!');
-}
-else if( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
-  return('Wygrywa komputer!');
-}
-else if( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
-  return('Wygrywa komputer!');
-}
-else if( argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch'){
-  return('TVARDY weź się nie wygłupiaj. Wybierz swój ruch: kamień, papier lub nożyce. Tu się nie gra kaloryferem, krzesłem ...ani śrubokrętem');
-}
-else if( argComputerMove == 'nożyce' && argPlayerMove == 'nieznany ruch'){
-  return('TVARDY weź się nie wygłupiaj. Wybierz swój ruch: kamień, papier lub nożyce. Tu się nie gra kaloryferem, krzesłem ...ani śrubokrętem');
-}
-else if( argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch'){
-return('TVARDY weź się nie wygłupiaj. Wybierz swój ruch: kamień, papier lub nożyce. Tu się nie gra kaloryferem, krzesłem ...ani śrubokrętem');
-}
-}
-/*printMessage('Komputer zagrał '+argComputerMove+', a Ty '+argPlayerMove);*/
-printMessage ('wynik: '+ displayResult(argComputerMove, argPlayerMove));
+playGame(3);
